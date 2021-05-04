9f6fe3ea64a96b4dcff45a740066119c5a4c2415
Ben Thomasson <bthomass@redhat.com>
2021-05-04 10:25:38 -0400

    Adds simple module tests

--- ansible/tests/test_1.py
+++ ansible/tests/test_1.py
@@ -1,4 +1,25 @@
 
 
+from subprocess import check_output
+import os
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+
+
 def test1():
-    pass
+    os.chdir(HERE)
+    output = check_output("ansible -i inventory.yml -m ping all".split(" "))
+    print(output.decode())
+    assert output
+
+def test2():
+    os.chdir(HERE)
+    output = check_output("ansible -M modules -i inventory.yml -m timetest all".split(" "))
+    print(output.decode())
+    assert output
+
+def test3():
+    os.chdir(HERE)
+    output = check_output("ansible -M modules -i inventory.yml -m argtest all".split(" "))
+    print(output.decode())
+    assert output
