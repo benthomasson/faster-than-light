bce6d817f0137eede36710d38e79460aefd76799
Ben Thomasson <bthomass@redhat.com>
2021-05-05 06:25:08 -0400

    Adds inventory, runs tasks on hosts concurrently

--- python-asyncio/faster_than_light/tests/test_1.py
+++ python-asyncio/faster_than_light/tests/test_1.py
@@ -6,13 +6,22 @@ import asyncio
 import tempfile
 import shutil
 import json
+import yaml
 from pprint import pprint
 
 HERE = os.path.dirname(os.path.abspath(__file__))
 
 
-async def run_module(module_dirs, module_name):
+def load_inventory(inventory_file):
 
+    with open(inventory_file) as f:
+        inventory_data = yaml.safe_load(f.read())
+    return inventory_data
+
+
+def find_module(module_dirs, module_name):
+
+    # Find the module in module_dirs
     for d in module_dirs:
         module = os.path.join(d, f'{module_name}.py')
         if os.path.exists(module):
@@ -20,17 +29,63 @@ async def run_module(module_dirs, module_name):
         else:
             module = None
 
+    return module
+
+
+async def run_module_on_host(host_name, host, module):
+    if host.get('ansible_connection') == 'local':
+        tmp = tempfile.mkdtemp()
+        tmp_module = os.path.join(tmp, 'module.py')
+        shutil.copy(module, tmp_module)
+        # TODO: replace hashbang with ansible_python_interpreter
+        # TODO: add utf-8 encoding line
+        args = os.path.join(tmp, 'args')
+        with open(args, 'w') as f:
+            f.write('some args')
+        output = await check_output(f'{tmp_module} {args}')
+        return host_name, json.loads(output)
+    else:
+        # Remote connection
+        # TODO: Implement remote connection
+        return host_name, None
+
+
+async def run_module(inventory, module_dirs, module_name):
+
+    '''
+    Runs a module on all items in an inventory concurrently.
+    '''
+
+    module = find_module(module_dirs, module_name)
+
     if module is None:
         raise Exception('Module not found')
 
-    tmp = tempfile.mkdtemp()
-    tmp_module = os.path.join(tmp, 'module.py')
-    shutil.copy(module, tmp_module)
-    args = os.path.join(tmp, 'args')
-    with open(args, 'w') as f:
-        f.write('some args')
-    output =  await check_output(f'{tmp_module} {args}')
-    return json.loads(output)
+    hosts = dict()
+
+    tasks = []
+
+    # Make a set of unique hosts
+    for group_name, group in inventory.items():
+        for host_name, host in group.get('hosts').items():
+            hosts[host_name] = host
+
+    # Create a set of tasks to run concurrently
+    for host_name, host in hosts.items():
+        tasks.append(asyncio.create_task(run_module_on_host(host_name, host, module)))
+
+    # Await all the tasks
+    await asyncio.gather(*tasks)
+
+    # Extract results
+    results = {}
+    for task in tasks:
+        host_name, result = task.result()
+        print(host_name)
+        print(result)
+        results[host_name] = result
+
+    return results
 
 
 async def check_output(cmd):
@@ -54,14 +109,14 @@ async def test_1():
 @pytest.mark.asyncio
 async def test_2():
     os.chdir(HERE)
-    output = await run_module(['modules'], 'timetest')
+    output = await run_module(load_inventory('inventory.yml'), ['modules'], 'timetest')
     pprint(output)
-    assert output
+    assert output['localhost']
 
 
 @pytest.mark.asyncio
 async def test_3():
     os.chdir(HERE)
-    output = await run_module(['modules'], 'argtest')
+    output = await run_module(load_inventory('inventory.yml'), ['modules'], 'argtest')
     pprint(output)
-    assert output
+    assert output['localhost']
