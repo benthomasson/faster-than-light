acc32c2633567c2b5e2c78d2ec9d4fbb59d5393a
Ben Thomasson <bthomass@redhat.com>
2021-05-08 05:09:36 -0400

    Adds running modules through the gate

--- python-asyncio/faster_than_light/tests/test_ftl_gate.py
+++ python-asyncio/faster_than_light/tests/test_ftl_gate.py
@@ -9,6 +9,8 @@ import tempfile
 import zipapp
 import shutil
 import base64
+import asyncssh
+from pprint import pprint
 
 from .test_1 import find_module
 
@@ -22,10 +24,25 @@ def send_message(writer, msg_type, msg_data):
     writer.write('{:08x}'.format(len(message)).encode())
     writer.write(message)
 
+
+def send_message_str(writer, msg_type, msg_data):
+    message = json.dumps([msg_type, msg_data])
+    print('{:08x}'.format(len(message)))
+    print(message)
+    writer.write('{:08x}'.format(len(message)))
+    writer.write(message)
+
+
 async def read_message(reader):
-    length = await reader.read(8)
-    value = await reader.read(int(length, 16))
-    return json.loads(value)
+    while True:
+        length = await reader.read(8)
+        try:
+            value = await reader.read(int(length, 16))
+        except ValueError:
+            print(f'length {length}')
+            raise
+        return json.loads(value)
+
 
 def build_ftl_gate():
 
@@ -36,6 +53,40 @@ def build_ftl_gate():
                           sys.executable)
     shutil.rmtree(os.path.join(tempdir, 'ftl_gate'))
     return os.path.join(tempdir, 'ftl_gate.pyz')
+
+
+async def run_module_on_host(host_name, host, module):
+    if host and host.get('ansible_connection') == 'local':
+        raise NotImplementedError()
+    else:
+        # Remote connection
+        # TODO: Implement remote connection
+        module_name = os.path.basename(module)
+        async with asyncssh.connect(host_name) as conn:
+            try:
+                ftl_gate = build_ftl_gate()
+                result = await conn.run('mkdir /tmp/ftl', check=True)
+                print(result.exit_status)
+                result = await conn.run('touch /tmp/ftl/args', check=True)
+                print(result.exit_status)
+                async with conn.start_sftp_client() as sftp:
+                    await sftp.put(ftl_gate, '/tmp/ftl/')
+                result = await conn.run('chmod 700 /tmp/ftl/ftl_gate.pyz', check=True)
+                print(result.exit_status)
+                process = await conn.create_process('/tmp/ftl/ftl_gate.pyz')
+                send_message_str(process.stdin, "Hello", {})
+                assert await read_message(process.stdout) == ['Hello', {}]
+                with open(module, 'rb') as f:
+                    module_text = base64.b64encode(f.read()).decode()
+                send_message_str(process.stdin, 'Module', dict(module=module_text, module_name=module_name))
+                return host_name, await read_message(process.stdout)
+            finally:
+                send_message_str(process.stdin, "Shutdown", {})
+                if process.exit_status is not None:
+                    print(await process.stderr.read())
+                result = await conn.run('rm -rf /tmp/ftl', check=True)
+                print(result.exit_status)
+        return host_name, None
 
 
 @pytest.mark.asyncio
@@ -57,6 +108,7 @@ async def test_build_ftl_gate():
         send_message(proc.stdin, 'Shutdown', {})
         await proc.wait()
         os.unlink(ftl_gate)
+
 
 @pytest.mark.asyncio
 async def test_run_module():
@@ -86,6 +138,7 @@ async def test_run_module():
         await proc.wait()
         os.unlink(ftl_gate)
 
+
 @pytest.mark.asyncio
 async def test_run_ftl_module():
     os.chdir(HERE)
@@ -113,3 +166,14 @@ async def test_run_ftl_module():
         send_message(proc.stdin, 'Shutdown', {})
         await proc.wait()
         os.unlink(ftl_gate)
+
+
+@pytest.mark.asyncio
+async def test_run_module_on_host():
+    os.chdir(HERE)
+    hostname, output = await run_module_on_host('localhost',
+                                                dict(),
+                                                os.path.join(HERE, 'modules', 'argtest.py'))
+    pprint(output)
+    assert hostname == 'localhost'
+    assert output[0] == 'ModuleResult'
