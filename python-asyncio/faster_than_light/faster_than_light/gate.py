11a338235309db9bcd10828d1234354c9034eb83
Ben Thomasson <bthomass@redhat.com>
2021-05-09 06:06:21 -0400

    Uses importlib_resources to build the ftl_gate

--- python-asyncio/faster_than_light/faster_than_light/gate.py
+++ python-asyncio/faster_than_light/faster_than_light/gate.py
@@ -4,14 +4,16 @@ import sys
 import tempfile
 import zipapp
 import shutil
-
-HERE = os.path.dirname(os.path.abspath(__file__))
+from importlib_resources import files
+import faster_than_light.ftl_gate
 
 
 def build_ftl_gate():
 
     tempdir = tempfile.mkdtemp()
-    shutil.copytree(os.path.join(HERE, 'ftl_gate'), os.path.join(tempdir, 'ftl_gate'))
+    os.mkdir(os.path.join(tempdir, 'ftl_gate'))
+    with open(os.path.join(tempdir, 'ftl_gate', '__main__.py'), 'w') as f:
+        f.write(files(faster_than_light.ftl_gate).joinpath('__main__.py').read_text())
     zipapp.create_archive(os.path.join(tempdir, 'ftl_gate'),
                           os.path.join(tempdir, 'ftl_gate.pyz'),
                           sys.executable)
