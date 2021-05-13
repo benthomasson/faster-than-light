e70183b8c298038aa79fb45e5d70558b3a2dc09d
Ben Thomasson <bthomass@redhat.com>
2021-05-13 09:52:54 -0400

    Adds performance test results chart

--- performance-test/python-asyncio/run_perf.sh
+++ performance-test/python-asyncio/run_perf.sh
@@ -6,7 +6,7 @@ for t in argtest
 do
 for n in 1 10 100 1000
 do
-./perf_test.py "ftl -i inventory${n}.yml -M modules -m ${t}" ${t}_${n} ${t} ${n} --no-header
-./perf_test.py "ftl -i inventory${n}.yml -M ftl_modules -f ${t}" ftl_${t}_${n} ftl_${t} ${n} --no-header
+./perf_test.py "ftl -i inventory${n}.yml -M modules -m ${t}" ftl ${t} ${n} --no-header
+./perf_test.py "ftl -i inventory${n}.yml -M ftl_modules -f ${t}" ftl_async ${t} ${n} --no-header
 done
 done
