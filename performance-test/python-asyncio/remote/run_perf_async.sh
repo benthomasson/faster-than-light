02e184cf11ffcf15a253727e44406145b046d0b2
Ben Thomasson <bthomass@redhat.com>
2021-05-14 08:58:39 -0400

    Refactors module, enables remote ftl module execution

---
+++ performance-test/python-asyncio/remote/run_perf_async.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+ulimit -Sn 10000
+#Argtest
+
+../../scripts/perf_test.py x x x x --only-header
+for t in argtest
+do
+for n in 1 10 100 1000
+do
+#../../scripts/perf_test.py "ftl -i inventory_remote${n}.yml -M modules -m ${t}" ftl_remote ${t} ${n} --no-header
+../../scripts/perf_test.py "ftl -i inventory_remote${n}.yml -M ftl_modules -f ${t}" ftl_async_remote ${t} ${n} --no-header
+done
+done
--- python-asyncio/faster_than_light/faster_than_light/module.py
+++ python-asyncio/faster_than_light/faster_than_light/module.py
@@ -36,21 +36,67 @@ def find_module(module_dirs, module_name):
     return module
 
 
-async def run_module_on_host(host_name, host, module):
+async def send_gate(conn, tempdir):
+    ftl_gate = build_ftl_gate()
+    async with conn.start_sftp_client() as sftp:
+        await sftp.put(ftl_gate, f'{tempdir}/')
+    result = await conn.run(f'chmod 700 {tempdir}/ftl_gate.pyz', check=True)
+    assert result.exit_status == 0
+
+
+async def open_gate(conn, tempdir):
+    process = await conn.create_process(f'{tempdir}/ftl_gate.pyz')
+    send_message_str(process.stdin, "Hello", {})
+    assert await read_message(process.stdout) == ['Hello', {}]
+    return process
+
+
+async def run_module_through_gate(gate_process, module, module_name):
+    with open(module, 'rb') as f:
+        module_text = base64.b64encode(f.read()).decode()
+    send_message_str(gate_process.stdin, 'Module', dict(module=module_text, module_name=module_name))
+    return await read_message(gate_process.stdout)
+
+
+async def run_ftl_module_through_gate(gate_process, module, module_name):
+    with open(module, 'rb') as f:
+        module_text = base64.b64encode(f.read()).decode()
+    send_message_str(gate_process.stdin, 'FTLModule', dict(module=module_text, module_name=module_name))
+    return await read_message(gate_process.stdout)
+
+
+async def run_module_locally(host_name, host, module):
+    tmp = tempfile.mkdtemp()
+    tmp_module = os.path.join(tmp, 'module.py')
+    shutil.copy(module, tmp_module)
+    # TODO: replace hashbang with ansible_python_interpreter
+    # TODO: add utf-8 encoding line
+    args = os.path.join(tmp, 'args')
+    with open(args, 'w') as f:
+        f.write('some args')
+    output = await check_output(f'{tmp_module} {args}')
+    return host_name, json.loads(output)
+
+
+async def run_ftl_module_locally(host_name, host, module_path):
+
+    with open(module_path, 'rb') as f:
+        module_compiled = compile(f.read(), module_path, 'exec')
+
+    globals = {'__file__': module_path}
+    locals = {}
+
+    exec(module_compiled, globals, locals)
+    result = await locals['main']()
+    return host_name, result
+
+
+async def run_module_on_host(host_name, host, module, local_runner, remote_runner):
     if host and host.get('ansible_connection') == 'local':
-        tmp = tempfile.mkdtemp()
-        tmp_module = os.path.join(tmp, 'module.py')
-        shutil.copy(module, tmp_module)
-        # TODO: replace hashbang with ansible_python_interpreter
-        # TODO: add utf-8 encoding line
-        args = os.path.join(tmp, 'args')
-        with open(args, 'w') as f:
-            f.write('some args')
-        output = await check_output(f'{tmp_module} {args}')
-        return host_name, json.loads(output)
+        return await local_runner(host_name, host, module)
     else:
         module_name = os.path.basename(module)
-        if host.get('ansible_host'):
+        if host and host.get('ansible_host'):
             ssh_host = host.get('ansible_host')
         else:
             ssh_host = host_name
@@ -60,30 +106,21 @@ async def run_module_on_host(host_name, host, module):
                     process = None
                     try:
                         tempdir = f'/tmp/ftl-{uuid.uuid4()}'
-                        ftl_gate = build_ftl_gate()
                         result = await conn.run(f'mkdir {tempdir}', check=True)
-                        #print(result.exit_status)
                         result = await conn.run(f'touch {os.path.join(tempdir, "args")}', check=True)
-                        #print(result.exit_status)
-                        async with conn.start_sftp_client() as sftp:
-                            await sftp.put(ftl_gate, f'{tempdir}/')
-                        result = await conn.run(f'chmod 700 {tempdir}/ftl_gate.pyz', check=True)
-                        #print(result.exit_status)
-                        process = await conn.create_process(f'{tempdir}/ftl_gate.pyz')
-                        send_message_str(process.stdin, "Hello", {})
-                        assert await read_message(process.stdout) == ['Hello', {}]
-                        with open(module, 'rb') as f:
-                            module_text = base64.b64encode(f.read()).decode()
-                        send_message_str(process.stdin, 'Module', dict(module=module_text, module_name=module_name))
-                        return host_name, await read_message(process.stdout)
+                        assert result.exit_status == 0
+                        await send_gate(conn, tempdir)
+                        gate_process = await open_gate(conn, tempdir)
+                        return host_name, await remote_runner(gate_process, module, module_name)
                     finally:
                         if process is not None:
                             send_message_str(process.stdin, "Shutdown", {})
                         if process is not None and process.exit_status is not None:
-                            #print(await process.stderr.read())
+                            # print(await process.stderr.read())
                             await process.stderr.read()
                         result = await conn.run(f'rm -rf {tempdir}', check=True)
-                    #print(result.exit_status)
+                        assert result.exit_status == 0
+                    # print(result.exit_status)
                 break
             except ConnectionResetError:
                 print('retry connection')
@@ -91,59 +128,6 @@ async def run_module_on_host(host_name, host, module):
                 continue
 
         return host_name, None
-
-
-async def run_module(inventory, module_dirs, module_name):
-
-    '''
-    Runs a module on all items in an inventory concurrently.
-    '''
-
-    module = find_module(module_dirs, module_name)
-
-    if module is None:
-        raise Exception('Module not found')
-
-    # Make a set of unique hosts
-
-    hosts = unique_hosts(inventory)
-
-    # Create a set of tasks to run concurrently
-    all_tasks = []
-    for c in chunk(list(hosts.items()), 10):
-        tasks = []
-        for host_name, host in c:
-            tasks.append(asyncio.create_task(run_module_on_host(host_name, host, module)))
-        await asyncio.gather(*tasks)
-        all_tasks.extend(tasks)
-
-    return extract_task_results(all_tasks)
-
-
-async def run_ftl_module(inventory, module_dirs, module_name):
-
-    '''
-    Runs a module on all items in an inventory concurrently.
-    '''
-
-    module = find_module(module_dirs, module_name)
-
-    if module is None:
-        raise Exception('Module not found')
-
-    # Make a set of unique hosts
-
-    hosts = unique_hosts(inventory)
-
-    # Create a set of tasks to run concurrently
-    tasks = []
-    for host_name, host in hosts.items():
-        tasks.append(asyncio.create_task(run_ftl_module_on_host(host_name, host, module)))
-
-    # Await all the tasks
-    await asyncio.gather(*tasks)
-
-    return extract_task_results(tasks)
 
 
 def unique_hosts(inventory):
@@ -159,29 +143,46 @@ def unique_hosts(inventory):
 
 def extract_task_results(tasks):
 
-    # Extract results
     results = {}
     for task in tasks:
         host_name, result = task.result()
-        #print(host_name)
-        #print(result)
         results[host_name] = result
     return results
 
 
-async def run_ftl_module_on_host(hostname, host, module_path):
+async def _run_module(inventory, module_dirs, module_name, local_runner, remote_runner):
 
-    if host and host.get('ansible_connection') == 'local':
-        with open(module_path, 'rb') as f:
-            module_compiled = compile(f.read(), module_path, 'exec')
+    module = find_module(module_dirs, module_name)
 
-        globals = {'__file__': module_path}
-        locals = {}
+    if module is None:
+        raise Exception('Module not found')
 
-        exec(module_compiled, globals, locals)
-        result = await locals['main']()
-        return hostname, result
-    else:
-        # TODO: implement remote execution
-        raise NotImplementedError("implement remote execution for ftl modules")
-        return hostname, None
+    hosts = unique_hosts(inventory)
+
+    all_tasks = []
+    # Run the tasks in chunks of 10 to reduce contention for remote connections.
+    # This doubles performance at num_hosts=1000 for remote execution
+    for c in chunk(list(hosts.items()), 10):
+        tasks = []
+        for host_name, host in c:
+            tasks.append(asyncio.create_task(run_module_on_host(host_name, host, module, local_runner, remote_runner)))
+        await asyncio.gather(*tasks)
+        all_tasks.extend(tasks)
+
+    return extract_task_results(all_tasks)
+
+
+async def run_module(inventory, module_dirs, module_name):
+    '''
+    Runs a module on all items in an inventory concurrently.
+    '''
+
+    return await _run_module(inventory, module_dirs, module_name, run_module_locally, run_module_through_gate)
+
+
+async def run_ftl_module(inventory, module_dirs, module_name):
+    '''
+    Runs a module on all items in an inventory concurrently.
+    '''
+
+    return await _run_module(inventory, module_dirs, module_name, run_ftl_module_locally, run_ftl_module_through_gate)
