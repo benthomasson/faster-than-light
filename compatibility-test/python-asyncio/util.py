6b826e51524d7f815424db9967b594396378e3b8
Ben Thomasson <bthomass@redhat.com>
2022-06-27 19:42:14 -0400

    Adds helper functions for building module paths

--- compatibility-test/python-asyncio/util.py
+++ compatibility-test/python-asyncio/util.py
@@ -54,3 +54,41 @@ def find_module(name: str):
 
 
 
+def find_builtin_modules_path():
+    output = subprocess.check_output([find_ansible(), '--version'])
+    for line in output.splitlines():
+        line = line.decode()
+        line = line.strip()
+        if line.startswith('ansible python module location ='):
+            _, _, ansible_location = line.partition('=')
+            ansible_location = ansible_location.strip()
+            modules_location = os.path.join(ansible_location, 'modules')
+
+    return modules_location
+
+def find_collection_modules_paths():
+    module_paths = []
+    output = subprocess.check_output([find_galaxy(), 'collection', 'list'])
+    output_lines = [line.decode() for line in output.splitlines()]
+    line = output_lines.pop(0) if output_lines else None
+    while output_lines or line:
+        if line.startswith('# '):
+            location = line[2:]
+            if os.path.exists(location):
+                line = output_lines.pop(0) if output_lines else None
+                while output_lines or line:
+                    if line.startswith('# '):
+                        break
+                    collection, _, _ = line.partition(' ')
+                    collection_parts = collection.split('.')
+                    collection_location = os.path.join(location, *collection_parts)
+                    if os.path.exists(os.path.join(collection_location, 'plugins', 'modules')):
+                        module_paths.append(os.path.join(collection_location, 'plugins', 'modules'))
+                    line = output_lines.pop(0) if output_lines else None
+        line = output_lines.pop(0) if output_lines else None
+    return module_paths
+
+def build_module_paths():
+    module_paths = [find_builtin_modules_path()]
+    module_paths.extend(find_collection_modules_paths())
+    return module_paths
