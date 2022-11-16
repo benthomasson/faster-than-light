31ca709803e480b90d63a5065b5ede0b032faa70
Ben Thomasson <ben.thomasson@gmail.com>
2022-11-15 22:23:57 -0500

    Bump version: 0.1.2 → 0.1.3

--- python-asyncio/faster_than_light/faster_than_light/__init__.py
+++ python-asyncio/faster_than_light/faster_than_light/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Ben Thomasson"""
 __email__ = 'benthomasson@gmail.com'
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 from .module import run_module, run_ftl_module
 from .ssh import close_gate
